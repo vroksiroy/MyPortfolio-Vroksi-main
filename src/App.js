557787024afdfad1da1c7618cb382de59ddc3f41
vroksiroy <royvroksi@gmail.com>
2025-05-28 15:56:02 +0530

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
-import Education from "./components/Education";
+// import Education from "./components/Education";
 import ProjectDetails from "./components/ProjectDetails";
 import styled from "styled-components";
 
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+// import emailjs from '@emailjs/browser';
 import { Snackbar } from '@mui/material';
 
 const Container = styled.div`
